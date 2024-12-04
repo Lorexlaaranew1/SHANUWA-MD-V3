dc396c46c9dbf643aa0db9eee838b134f2646b80
Lorexlaaranew1 <Vishmithathisara223@gmail.com>
2024-12-04 14:04:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/2qD0Syx/6648.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
 SUDO_NB: process.env.SUDO_NB || "94774609569",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 AUTO_VOICE:"true"
 };
 
